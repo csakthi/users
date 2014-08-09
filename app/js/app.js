21b38af0a9f8147457027a91968446cd67d56996
sakthi <csakthi@gmail.com>
2014-08-08 22:55:44 -0400

    add

--- app/js/app.js
+++ app/js/app.js
@@ -11,6 +11,23 @@ define([
 
     var initialize = function(){
         // Pass in our Router module and call it's initialize function
-       var router = new Router();
+        var router = new Router();
+        <!-- activated for home (index.html) in the URL -->
+        router.on('route:home', function () {
+            console.log("hey! home page");
+            console.log(UserListView);
+            var userListView = new UserListView();
+            userListView.render();
+        });
+
+        <!-- activated for url "#/new" in the URL -->
+        <!-- id field is bind automatically if edit with id present in the url -->
+        <!-- for url #new, id field will be undefined -->
+        router.on('route:editUser', function (id) {
+            <!-- renders the edit user form-->
+            console.log("hey! edit user form");
+            var editUserView = new EditUserView();
+            editUserView.render({id: id});
+        });
     };
 });
--- app/js/router.js
+++ app/js/router.js
@@ -42,27 +42,6 @@ define([
         return o;
     };
 
-    var initialize = function () {
-        var router = new Router();
-        <!-- activated for home (index.html) in the URL -->
-        router.on('route:home', function () {
-            console.log("hey! home page");
-            console.log(UserListView);
-            var userListView = new UserListView();
-            userListView.render();
-        });
-
-        <!-- activated for url "#/new" in the URL -->
-        <!-- id field is bind automatically if edit with id present in the url -->
-        <!-- for url #new, id field will be undefined -->
-        router.on('route:editUser', function (id) {
-            <!-- renders the edit user form-->
-            console.log("hey! edit user form");
-            var editUserView = new EditUserView();
-            editUserView.render({id: id});
-        });
-
-        Backbone.history.start();
-    };
-
+    Backbone.history.start();
+    return Router;
 });
